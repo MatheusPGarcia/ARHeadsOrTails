5ea74d1a78851fac9cbd1533681e17ef96b16b59
Unknown <matheuspgarcia@gmail.com>
2018-07-18 17:40:00 -0300

    Detects horizontal plane and plot a ship in the plan

--- HeadsOrTails/HeadsOrTails/ViewController.swift
+++ HeadsOrTails/HeadsOrTails/ViewController.swift
@@ -7,19 +7,134 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var arSceneView: ARSCNView!
+
+    var coinNode: SCNNode?
+    var startPosition: SCNVector3?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        configureLighting()
+        addTapGestureToSceneView()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setUpSceneView()
     }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        arSceneView.session.pause()
+    }
 
+    func setUpSceneView() {
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+
+        arSceneView.session.run(configuration)
+
+        arSceneView.delegate = self
+        arSceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+    }
+
+    func configureLighting() {
+        arSceneView.autoenablesDefaultLighting = true
+        arSceneView.automaticallyUpdatesLighting = true
+    }
+
+    func addTapGestureToSceneView() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.handleTap(withGestureRecognizer:)))
+        arSceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+
+    @objc func handleTap(withGestureRecognizer recognizer: UIGestureRecognizer) {
+
+        guard let coin = coinNode else {
+            addShipToSceneView(recognizer: recognizer)
+            return
+        }
+
+        print("you already have a coin")
+    }
+
+    func addShipToSceneView(recognizer: UIGestureRecognizer) {
+
+        let tapLocation = recognizer.location(in: arSceneView)
+        let hitTestResults = arSceneView.hitTest(tapLocation, types: .existingPlaneUsingExtent)
+
+        guard let hitTestResult = hitTestResults.first else { return }
+        let translation = hitTestResult.worldTransform.translation
+        let x = translation.x
+        let y = translation.y
+        let z = translation.z
+
+        guard let coinScene = SCNScene(named: "ship.scn"),
+            let coinNode = coinScene.rootNode.childNode(withName: "ship", recursively: false)
+            else { return }
+
+        coinNode.position = SCNVector3(x,y,z)
+
+        self.coinNode = coinNode
+        arSceneView.scene.rootNode.addChildNode(coinNode)
+    }
 }
 
+extension ViewController: ARSCNViewDelegate {
+
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+
+        let width = CGFloat(planeAnchor.extent.x)
+        let height = CGFloat(planeAnchor.extent.z)
+        let plane = SCNPlane(width: width, height: height)
+
+        plane.materials.first?.diffuse.contents = UIColor.transparentLightBlue
+
+        let planeNode = SCNNode(geometry: plane)
+
+        let x = CGFloat(planeAnchor.center.x)
+        let y = CGFloat(planeAnchor.center.y)
+        let z = CGFloat(planeAnchor.center.z)
+
+        planeNode.position = SCNVector3(x, y, z)
+        planeNode.eulerAngles.x = -.pi/2
+
+        node.addChildNode(planeNode)
+    }
+
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor,
+            let planeNode = node.childNodes.first,
+            let plane = planeNode.geometry as? SCNPlane else { return }
+
+        let width = CGFloat(planeAnchor.extent.x)
+        let height = CGFloat(planeAnchor.extent.z)
+        plane.width = width
+        plane.height = height
+
+        let x = CGFloat(planeAnchor.center.x)
+        let y = CGFloat(planeAnchor.center.y)
+        let z = CGFloat(planeAnchor.center.z)
+        planeNode.position = SCNVector3(x, y, z)
+    }
+}
+
+extension float4x4 {
+    var translation: float3 {
+        let translation = self.columns.3
+        return float3(translation.x, translation.y, translation.z)
+    }
+}
+
+extension UIColor {
+    open class var transparentLightBlue: UIColor {
+        return UIColor(red: 90/255, green: 200/255, blue: 250/255, alpha: 0.50)
+    }
+}
